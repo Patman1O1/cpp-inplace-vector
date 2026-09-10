870c92f9e659653471db30dabf28d455668c1739
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-10 12:34:25 -0500

    Edit inplace_vector_test.cpp

--- tests/inplace_vector_test.cpp
+++ tests/inplace_vector_test.cpp
@@ -3,8 +3,429 @@
 #include <gmock/gmock.h>
 
 // Local Includes
-#include <collections/inplace_vector.hpp>
+//#include <collections/vector.hpp>
+#include <vector> // temporary
 
-namespace inplace_vector_testing {
+namespace collections::vector_testing {
+    // ── Constructor Tests ───────────────────────────────────────────────────
+    namespace constructor_testing {
+        // ── Default Constructor Tests ───────────────────────────────────────
+        namespace default_ctor_testing {
+            // Signature:
+            // constexpr vector() noexcept(noexcept(Allocator()))
 
-} // namespace inplace_vector_testing
+        } // namespace default_ctor_testing
+
+        // ── Allocator Constructor Tests ─────────────────────────────────────
+        namespace allocator_ctor_testing {
+            // Signature:
+            // explicit constexpr vector(const Allocator&) noexcept
+
+        } // namespace allocator_ctor_testing
+
+        // ── Count Constructor Tests ─────────────────────────────────────────
+        namespace count_ctor_testing {
+            // Signature:
+            // explicit constexpr vector(
+            //      size_type count,
+            //      const Allocator& alloc = Allocator()
+            // ) noexcept
+
+        } // namespace count_ctor_testing
+
+        // ── Count & Value Constructor Tests ─────────────────────────────────
+        namespace count_and_value_ctor_testing {
+            // Signature:
+            // constexpr vector(
+            //      size_type count,
+            //      const T& value,
+            //      const Allocator& alloc = Allocator()
+            // )
+
+        } // namespace count_and_value_ctor_testing
+
+        // ── Input Iterator Constructor Tests ────────────────────────────────
+        namespace input_iterator_ctor_testing {
+            // Signature:
+            // template<std::input_iterator InputIt>
+            // constexpr vector(
+            //      InputIt first,
+            //      InputIn last,
+            //      const Allocator& alloc = Allocator()
+            // )
+
+        } // namespace input_iterator_ctor_testing
+
+        // ── Range Constructor Tests ─────────────────────────────────────────
+        namespace range_ctor_testing {
+            // Signature:
+            // template<typename R> requires(
+            //      std::ranges::input_range<R> &&
+            //      std::convertible_to<
+            //          std::ranges::range_reference_t<R>,
+            //          T
+            //      )
+            // constexpr vector(
+            //      std::from_range_t,
+            //      R&& rg,
+            //      const Allocator& alloc = Allocator()
+            // )
+
+        } // namespace range_ctor_testing
+
+        // ── Copy Constructor Tests ──────────────────────────────────────────
+        namespace copy_ctor_testing {
+            // Signature:
+            // constexpr vector(const vector&)
+
+        } // namespace copy_ctor_testing
+       
+        // ── Move Constructor Tests ──────────────────────────────────────────
+        namespace move_ctor_testing {
+            // Signature:
+            // constexpr vector(vector&&) noexcept
+
+        } // namespace move_ctor_testing
+
+        // ── Copy Allocator Constructor Tests ────────────────────────────────
+        namespace copy_alloc_ctor_testing {
+            // Signature:
+            // constexpr vector(
+            //      const vector&,
+            //      const std::type_indentity_t<Allocator>&
+            // )
+            
+        } // namespace copy_alloc_ctor_testing
+
+        // ── Move Allocator Constructor Tests ────────────────────────────────
+        namespace move_alloc_ctor_testing {
+            // Signature:
+            // constexpr vector(
+            //      vector&&,
+            //      const std::type_indentity_t<Allocator>&
+            // )
+
+        } // namespace move_alloc_ctor_testing
+
+        // ── Initializer List Constructor Tests ──────────────────────────────
+        namespace init_list_ctor_testing {
+            // Signature:
+            // vector(
+            //      std::initializer_list<T> init,
+            //      const Allocator&
+            // )
+        } // namespace init_list_ctor_testing
+    } // namespace constructor_testing
+
+    // ── Destructor Tests ────────────────────────────────────────────────────
+    namespace destructor {
+        // Signature:
+        // constexpr ~vector()
+
+    } // namespace destructor
+    
+    // ── Operator Tests ──────────────────────────────────────────────────────
+    namespace operators {
+        // ── Copy Assignment Operator Tests ──────────────────────────────────
+        namespace copy_assignment_operator {
+            // Signature:
+            // constexpr vector& operator=(const vector&)
+
+        } // namespace copy_assignment_operator
+
+        // ── Move Assignment Operator Tests ──────────────────────────────────
+        namespace move_assignment_operator {
+            // Signature:
+            // vector& operator=(vector&&) noexcept
+
+        } // namespace move_assignment_operator
+
+        // ── Equivalence Operator Tests ──────────────────────────────────────
+        namespace equivalence_operator {
+            // Signature:
+            // constexpr bool operator==(const vector<T, Allocator>&)
+        
+        } // namespace equivalence_operator
+        
+        // ── Three Way Comparison Operator Tests ─────────────────────────────
+        namespace three_way_comparison_operator {
+            // Signature:
+            // constexpr auto operator<=>(const vector<T, Allocator&)
+
+        } // namespace three_way_comparison_operator
+
+        // ── Random Access Operator Tests ────────────────────────────────────
+        namespace random_access_operator {
+            namespace mut_overload {
+                // Signature:
+                // constexpr reference operator[](size_type)
+
+            } // namespace mut_overload
+            
+            namespace const_overload {
+                // Signature:
+                // constexpr const_reference operator[](size_type) const
+
+            } // namespace const_overload
+        } // namespace random_access_operator
+    } // namespace operators
+
+    // ── Method Tests ────────────────────────────────────────────────────────
+    namespace methods {
+        namespace assign {
+            namespace count_and_value_overload {
+                // Signature:
+                // constexpr void assign(size_type count, const T&)
+
+            } // namespace count_and_value_overload
+            
+            namespace iterator_overload {
+                // Signature:
+                // template<std::input_iterator InputIt>
+                // constexpr void assign(InputIt first, InputIt last)
+
+            } // namespace iterator_overload
+
+            namespace init_list_overload {
+                // Signature:
+                // constexpr void assign(std::initializer_list<T>)
+
+            } // namespace init_list_overload
+        } // namespace assign
+
+        namespace assign_range {
+            // Signature:
+            // template<typename R> requires (
+            //      std::ranges::input_range<R> && 
+            //      std::convertible_to<std::ranges::range_reference_t<R>, T>
+            // )
+            // constexpr void assign_range(R&&)
+
+        } // namespace assign_range
+
+        namespace get_allocator {
+            // Signature:
+            // constexpr allocator_type get_allocator() const noexcept
+
+        } // namespace get_allocator
+
+        namespace at {
+            namespace mut_overload {
+                // Signature:
+                // constexpr reference at(size_type)
+                
+            } // namespace mut_overload
+            
+            namespace const_overload {
+                // Signature:
+                // constexpr const_reference at(size_type) const
+
+            } // namespace const_overload
+        } // namespace at
+
+        namespace front {
+            namespace mut_overload {
+                // Signature:
+                // constexpr reference front()
+
+            } // namespace mut_overload
+            
+            namespace const_overload {
+                // Signature:
+                // constexpr const_reference front() const
+
+            } // namespace const_overload
+        } // namespace front
+
+        namespace back {
+            namespace mut_overload {
+                // Signature:
+                // constexpr reference back()
+
+            } // namespace mut_overload
+
+            namespace const_overload {
+                // Signature:
+                // constexpr const_reference back() const
+
+            } // namespace const_overload
+        } // namespace back
+
+        namespace data {
+            namespace mut_overload {
+                // Signature:
+                // constexpr T* back()
+
+            } // namespace mut_overload
+
+            namespace const_overload {
+                // Signature:
+                // constexpr const T* back() const
+
+            } // namespace const_overload
+        } // namespace data
+
+        namespace begin {
+            namespace mut_overload {
+                // Signature:
+                // constexpr iterator begin() noexcept
+                
+            } // namespace mut_overload
+            
+            namespace const_overload {
+                // Signature:
+                // constexpr const_iterator begin() const noexcept
+
+            } // namespace const_overload
+        } // namespace begin
+
+        namespace cbegin {
+            // Signature:
+            // constexpr const_iterator cbegin() const noexcept
+
+        } // namespace cbegin
+
+        namespace end {
+            namespace mut_overload {
+                // Signature:
+                // constexpr iterator end() noexcept
+                
+            } // namespace mut_overload
+            
+            namespace const_overload {
+                // Signature:
+                // constexpr const_iterator end() const noexcept
+
+            } // namespace const_overload
+        } // namespace end
+
+        namespace cend {
+            // Signature:
+            // constexpr const_iterator cend() const noexcept
+
+        } // namespace cend
+
+        namespace empty {
+            // Signature:
+            // constexpr bool empty() const noexcept
+
+        } // namespace empty
+        
+        namespace size {
+            // Signature:
+            // constexpr size_type size() const noexcept
+
+        } // namespace size
+
+        namespace max_size {
+            // Signature:
+            // constexpr size_type max_size() const noexcept
+
+        } // namespace max_size
+
+        namespace reserve {
+            // Signature:
+            // constexpr void reserve()
+
+        } // namespace reserve
+
+        namespace capacity {
+            // Signature:
+            // constexpr size_type capacity() const noexcept
+
+        } // namespace capacity
+
+        namespace shrink_to_fit {
+            // Signature:
+            // constexpr void shrink_to_fit()
+
+        } // namespace shrink_to_fit
+
+        namespace clear {
+            // Signature:
+            // constexpr void clear() noexcept
+
+        } // namespace clear
+
+        namespace insert {
+            namespace copy_value_overload {
+                // Signature:
+                // constexpr iterator insert(const_iterator, const T&)
+
+            } // namespace copy_value_overload
+
+            namespace move_value_overload {
+                // Signature:
+                // constexpr iterator insert(const_iterator, T&&)
+
+            } // namespace move_value_overload
+
+            namespace copy_value_with_count_overload {
+                // Signature:
+                // constexpr iterator insert(
+                //      const_iterator,
+                //      size_type,
+                //      const T&
+                // )
+
+            } // namespace copy_value_with_count_overload
+
+            namespace input_iterator_overload {
+                // Signature:
+                // template<std::input_iterator InputIt>
+                // constexpr iterator insert(
+                //      const_iterator,
+                //      InputIt,
+                //      InputIt
+                // )
+
+            } // namespace input_iterator_overload
+
+            namespace init_list_overload {
+                // Signature:
+                // constexpr iterator insert(
+                //      const_iterator,
+                //      std::initializer_list<T>
+                // )
+
+            } // namespace init_list_overload
+        } // namespace insert
+
+        namespace input_range {
+            // Signature:
+            // template<typename R> requires(
+            //      std::ranges::input_range<R> && 
+            //      std::convertible_to<std::ranges::range_reference_t<R>, T>
+            // )
+            // constexpr iterator insert_range(const_iterator, R&&)
+
+        } // namespace input_range
+
+        namespace emplace {
+            // Signature:
+            // template<typename... Args>
+            // constexpr iterator emplace(const_iterator, Args&&...)
+
+        } // namespace emplace
+
+        namespace erase {
+            namespace single_iterator_overload {
+                // Signature:
+                // constexpr iterator erase(const_iterator)
+
+            } // namespace single_iterator_overload
+
+            namespace iterator_range_overload {
+                // Signature:
+                // constexpr iterator erase(const_iterator, const_iterator)
+
+            } // namespace iterator_range_overload
+        } // namespace erase
+
+    } // namespace methods
+
+    // ── Deduction Guide Tests ───────────────────────────────────────────────
+    namespace deduction_guide_testing {
+
+    } // namespace deduction_guide_testing
+} // namespace collections::vector_testing
